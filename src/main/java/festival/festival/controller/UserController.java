bdd49a47b88c79a7875f3fcc161eb4ed131959bf
Shande <jahirparedes670@gmail.com>
2025-05-05 20:21:36 -0500

    festival musica

---
+++ src/main/java/festival/festival/controller/UserController.java
@@ -0,0 +1,17 @@
+package festival.festival.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/")
+    public String userStart() {
+        return "festival";
+    }
+    @GetMapping("/contact")
+    public String contactUser() {
+        return "contact"; // This should map to contact.html in the templates folder
+    }
+}
---
+++ src/main/java/festival/festival/entity/User.java
@@ -0,0 +1,4 @@
+package festival.festival.entity;
+
+public class User {
+}
